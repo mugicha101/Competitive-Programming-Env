0b8497408bf48d7a32fe77f10ade27391c39a39c
Alexander Yoshida <alexander.yoshida@gmail.com>
2024-12-13 03:01:33 -0500

    remove extranous parts of radix trie

--- templates/radix_trie.cpp
+++ templates/radix_trie.cpp
@@ -1,42 +1,5 @@
 #define _USE_MATH_DEFINES
 #include <bits/stdc++.h>
- 
-using namespace std;
- 
-typedef unsigned int uint;
-typedef long long ll;
-typedef long double ld;
-typedef unsigned long long ull;
- 
-const int MOD = 1000000007;
-
-const ld ROT_HALF = 3.14159265358979323846L;
-const ld ROT_FULL = ROT_HALF * 2.L;
-const ld ROT_QUARTER = ROT_HALF * 0.5L;
-
-template <typename T,typename U>                                                   
-std::pair<T,U> operator+(const std::pair<T,U> & l,const std::pair<T,U> & r) {   
-    return {l.first+r.first,l.second+r.second};                                    
-}
-template <typename T,typename U>         
-std::pair<T,U> operator-(const std::pair<T,U> & l,const std::pair<T,U> & r) {   
-    return {l.first-r.first,l.second-r.second};                                    
-}
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-
-using namespace std;
-
-using ll = long long;
-const ll INF = LLONG_MAX >> 2;
-
-#define EPS 1e-7
-#define all(x) x.begin(), x.end()
-#define sz(x) ((int)x.size())
-#define each(i, x) for (auto &i : x)
-#define vcin(x) for (auto &_e : x) cin >> _e;
-#define mp(a,b) make_pair(a,b)
-
-#include <bits/stdc++.h>
 using namespace std;
 
 // compressed trie
@@ -118,33 +81,4 @@ struct TrieNode {
         e->end = mid;
         mid->insert(s, i + j);
     }
-
-    int traverse(int depth = 0) {
-        int sum = 0;
-        if (wordEnd) sum += depth;
-        for (auto &[c, e] : edges) { sum += e->end->traverse(depth + 1); }
-        return sum;
-    }
 };
-
-int main() {
-    // env setup
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    // cout.tie(0); // comment out for baekjoon
-    cout << fixed << setprecision(12);
-
-    cout << fixed << setprecision(2);
-    int n;
-    while (cin >> n) {
-        TrieNode *root = new TrieNode();
-        string s;
-        for (int i = 0; i < n; ++i) {
-            cin >> s;
-            root->insert(s);
-        }
-        int strokes = root->traverse();
-        cout << ((double)strokes / (double)n) << "\n";
-        delete root;
-    }
-}
