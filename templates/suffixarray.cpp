564b929445b5533878c718a570424459a4451b72
Alexander Yoshida <alexander.yoshida@gmail.com>
2024-12-14 22:36:19 -0500

    implement suffix array

--- templates/suffixarray.cpp
+++ templates/suffixarray.cpp
@@ -1,87 +1,64 @@
-/**
- * Author: Lukas Polacek
- * Date: 2009-10-27
- * License: CC0
- * Source: folklore and Linear-time longest-common-prefix
- * computation in suffix arrays and its applications (2001).
- * Description: Builds suffix array for a string. $a[i]$ is
- * the starting index of the suffix which is $i$-th in the
- * sorted suffix array. The returned vector is of size $n+1$,
- * and $a[0] = n$. The {\tt lcp} function calculates longest
- * common prefixes for neighbouring strings in suffix array.
- * The returned vector is of size $n+1$, and $ret[0] = 0$.
- * Time: $O(N \log^2 N)$ where $N$ is the length of the string
- * for creation of the SA. $O(N)$ for longest common prefixes.
- * Memory: $O(N)$
- * Status: Tested on UVa Live 4513
- */
-#pragma once
-
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
-using vi = vector<int>;
-#define sz(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
 
-typedef pair<ll, int> pli;
-void count_sort(vector<pli> &b, int bits) { // (optional)
-	//this is just 3 times faster than stl sort for N=10^6
-	int mask = (1 << bits) - 1;
-	for(int it = 0; it < 2; ++it) {
-		int move = it * bits;
-		vi q(1 << bits), w(sz(q) + 1);
-		for (int i = 0; i < sz(b); ++i)
-			q[(b[i].first >> move) & mask]++;
-		partial_sum(q.begin(), q.end(), w.begin() + 1);
-		vector<pli> res(b.size());
-		for(int i = 0; i < sz(b); ++i)
-			res[w[(b[i].first >> move) & mask]++] = b[i];
-		swap(b, res);
-	}
-}
 struct SuffixArray {
-	vi a;
-	string s;
-	SuffixArray(const string& _s) : s(_s + '\0') {
-		int N = sz(s);
-		vector<pli> b(N);
-		a.resize(N);
-		for(int i = 0; i < N; ++i) {
-			b[i].first = s[i];
-			b[i].second = i;
-		}
+    vector<vector<int>> c; // suffix equivalence classes
+    vector<int> p; // final level permutation
+    int lvls = 0;
+    SuffixArray(string s) {
+        int ogSize = (int)s.size();
+        while ((1 << lvls) <= ogSize) ++lvls;
+        s += '\0';
+        int n = s.size();
+        p.resize(n);
+        c.resize(lvls + 1, vector<int>(n));
 
-		int q = 8;
-		while ((1 << q) < N) q++;
-		for (int moc = 0;; moc++) {
-			count_sort(b, q); // sort(all(b)) can be used as well
-			a[b[0].second] = 0;
-			for(int i = 1; i < N; ++i)
-				a[b[i].second] = a[b[i - 1].second] +
-					(b[i - 1].first != b[i].first);
+        // sort single characters
+        iota(all(p), 0);
+        sort(all(p), [&](int i, int j) {
+            return s[i] < s[j];
+        });
+        c[0][p[0]] = 0;
+        for (int i = 1; i < n; ++i) {
+            c[0][p[i]] = c[0][p[i-1]] + (s[p[i-1]] != s[p[i]]);
+        }
 
-			if ((1 << moc) >= N) break;
-			for (int i = 0; i < N; ++i) {
-				b[i].first = (ll)a[i] << q;
-				if (i + (1 << moc) < N)
-					b[i].first += a[i + (1 << moc)];
-				b[i].second = i;
-			}
-		}
-		for(int i = 0; i < sz(a); ++i) a[i] = b[i].second;
-	}
-	vi lcp() {
-		// longest common prefixes: res[i] = lcp(a[i], a[i-1])
-		int n = sz(a), h = 0;
-		vi inv(n), res(n);
-		for (int i = 0; i < n; ++i) inv[a[i]] = i;
-		for (int i = 0; i < n; ++i) if (inv[i] > 0) {
-			int p0 = a[inv[i] - 1];
-			while (s[i + h] == s[p0 + h]) h++;
-			res[inv[i]] = h;
-			if(h > 0) h--;
-		}
-		return res;
-	}
+        // build up to full suffixes
+        auto modIdx = [&](int i) {
+            return i >= n ? i - n : i;
+        };
+        for (int l = 1; l <= lvls; ++l) {
+            int k = 1 << (l - 1);
+            auto cmp = [&](int i, int j) {
+                return c[l-1][i] == c[l-1][j] ? c[l-1][modIdx(i + k)] < c[l-1][modIdx(j + k)] : c[l-1][i] < c[l-1][j];
+            };
+            sort(all(p), cmp);
+            c[l][p[0]] = 0;
+            for (int i = 1; i < n; ++i) {
+                c[l][p[i]] = c[l][p[i-1]] + cmp(p[i-1], p[i]);
+            }
+        }
+    }
+
+    // lcp (lazy version, no lcp array)
+    int lcp(int i, int j) {
+        int ans = 0;
+        int n = p.size();
+        for (int l = lvls; l >= 0; --l) {
+            if (c[l][i % n] == c[l][j % n]) {
+                int k = 1 << l;
+                ans += k;
+                i += k;
+                j += k;
+            }
+        }
+        return ans;
+    }
+
+    // get kth suffix (start index) in alphabetical order
+    int kthSuffix(int k) {
+        return p[k + 1];
+    }
 };
